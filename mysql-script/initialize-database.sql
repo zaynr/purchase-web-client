35cdde2bd27eae458d4a0e9b1a69ac124e37d102
Zengyu Zeng <zayn@mail.ustc.edu.cn>
2017-09-01 14:00:52 +0800

    update table structure

--- mysql-script/initialize-database.sql
+++ mysql-script/initialize-database.sql
@@ -1,8 +1,62 @@
 CREATE DATABASE IF NOT EXISTS purchase;
 USE purchase;
+
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS providers;
+DROP TABLE IF EXISTS purchaser;
+DROP TABLE IF EXISTS order_types;
+DROP TABLE IF EXISTS pur_orders;
+DROP TABLE IF EXISTS pro_orders;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS contracts;
+
+#用户主表
 CREATE TABLE users(
 	user_name nvarchar(63) NOT NULL PRIMARY KEY,
     pwd varchar(255),
     user_type int	#0：管理员；1：采购商；2：供应商
+);
+#供应商
+CREATE TABLE providers(
+	user_name nvarchar(63) NOT NULL PRIMARY KEY,
+    mobile_no nvarchar(12),
+    provide_type nvarchar(255)
+);
+#采购商
+CREATE TABLE purchaser(
+	user_name nvarchar(63) NOT NULL PRIMARY KEY,
+    mobile_no nvarchar(12)
+);
+#订单类型
+CREATE TABLE order_types(
+	type_no int NOT NULL PRIMARY KEY,
+    type_content nvarchar(255)
+);
+#采购订单表
+CREATE TABLE pur_orders(
+	pur_serial_no int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    purchaser_name nvarchar(63),
+    order_status int,	#0：待接；1：已报价；2：已签；3：已完成
+    type_no int
+);
+#供应报价表
+CREATE TABLE pro_orders(
+    pro_serial_no int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	pur_serial_no int,
+    order_status int,	#0：待接；1：已报价；2：已签；3：已完成
+    provider_name nvarchar(63)
+);
+#联系人
+CREATE TABLE contacts(
+	serial_no int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    purchaser_name nvarchar(63),
+    provider_name nvarchar(63)
+);
+#合同表
+CREATE TABLE contracts(
+	contract_serial_no int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    pro_serial_no int,
+	pur_serial_no int,
+    contract_status int,	#0：正在进行；1：已完成
+    sign_time datetime
 );
---
+++ src/main/webapp/WEB-INF/jsp/script-sources.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zengzy19585
+  Date: 2017/9/1
+  Time: 11:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<script type="text/javascript" src="../../script/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="../../script/bootstrap.js"></script>
+<link rel="stylesheet" type="text/css" href="../../css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="../../css/bootstrap-grid.css"/>
+<link rel="stylesheet" type="text/css" href="../../css/bootstrap-reboot.css"/>
