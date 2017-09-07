bd770cf1f1bebb8cb2f0d462b3121ec4c77fc874
Zengyu Zeng <zayn@mail.ustc.edu.cn>
2017-09-07 18:41:00 +0800

    new data-script

--- mysql-script/base-data.sql
+++ mysql-script/base-data.sql
@@ -18,28 +18,7 @@ INSERT INTO `purchase`.`admin_option` (`option_no`, `option_describe`, `option_c
 INSERT INTO `purchase`.`admin_option` (`option_no`, `option_describe`, `option_content`) VALUES ('2', '需求每页条数', '10');
 INSERT INTO `purchase`.`admin_option` (`option_no`, `option_describe`, `option_content`) VALUES ('3', '默认页面条数', '10');
 
-INSERT INTO serial_no_gen(serial_no) VALUES('123');
-
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('0', '0', '0', '0', '0', '0', '0', '0', '0');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('1', '123', '0', '0', '12', '321', NULL, '1,2,3,', '6');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('2', '123', '1', '0', '123', '321', NULL, NULL, '5');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('3', '123', '2', '0', '123', '321', NULL, NULL, '4');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('4', '123', '3', '0', '123', '321', NULL, NULL, '3');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('5', '123', '4', '0', '123', '321', NULL, NULL, '2');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('7', '123', '7', '0', '123', '321', NULL, NULL, '4');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('8', '123', '8', '0', '12', '321', NULL, NULL, '3');
-INSERT INTO `purchase`.`pur_orders` (`pur_serial_no`, `purchaser_name`, `order_status`, `expect_status`, `order_amount`, `expect_price`, `more_detail`, `filter_dict`, `type_no`) VALUES ('9', '123', '0', '0', '12', '321', NULL, NULL, '3');
-
-INSERT INTO pro_orders(pro_serial_no, pur_serial_no, order_status, offer_price, provider_name) VALUES('10', '2', '1', '123', '321');
-INSERT INTO pro_orders(pro_serial_no, pur_serial_no, order_status, offer_price, provider_name, express_no) VALUES('11', '3', '2', '321', '321', 'A1B2C3E4');
-INSERT INTO pro_orders(pro_serial_no, pur_serial_no, order_status, offer_price, provider_name, express_no) VALUES('12', '4', '3', '321', '321', 'A1B2C3E4');
-INSERT INTO pro_orders(pro_serial_no, pur_serial_no, order_status, offer_price, provider_name, express_no) VALUES('13', '5', '4', '321', '321', 'A1B2C3E4');
-INSERT INTO pro_orders(pro_serial_no, pur_serial_no, order_status, offer_price, provider_name, express_no) VALUES('14', '5', '4', '312', '12321', 'C3E4A1B2');
-INSERT INTO pro_orders(pro_serial_no, pur_serial_no, order_status, offer_price, provider_name, express_no) VALUES('15', '6', '6', '321', '321', 'A1B2C3E4');
-
-INSERT INTO `purchase`.`filter_dict` (`order_serial_no`, `province`, `city`, `dist`) VALUES ('1', '浙江省', '杭州市', '滨江区');
-INSERT INTO `purchase`.`filter_dict` (`order_serial_no`, `province`, `city`, `dist`) VALUES ('1', '福建省', '泉州市', 'null');
-INSERT INTO `purchase`.`filter_dict` (`order_serial_no`, `province`, `city`, `dist`) VALUES ('1', '重庆市', 'null', 'null');
+INSERT INTO serial_no_gen(serial_no) VALUES('100');
 
 INSERT INTO order_types(type_content, type_unit) VALUES("原油", "桶");
 INSERT INTO order_types(type_content, type_unit) VALUES("糖果", "吨");
--- src/main/java/me/zengzy/repo/PurOrderRepository.java
+++ src/main/java/me/zengzy/repo/PurOrderRepository.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 @Repository
 @Transactional
 public interface PurOrderRepository extends CrudRepository<PurOrders, Long> {
-    //where FIND_IN_SET(order_type, :provide_type)
 
     @Query(value = "SELECT * FROM pur_orders WHERE purchaser_name = :purchaser_name", nativeQuery = true)
     ArrayList<PurOrders> getOrderByName(@Param("purchaser_name") String name);
@@ -24,7 +23,7 @@ public interface PurOrderRepository extends CrudRepository<PurOrders, Long> {
     @Query(value = "SELECT * FROM pur_orders WHERE purchaser_name = :purchaser_name AND order_status = :status", nativeQuery = true)
     ArrayList<PurOrders> getPurOrderByNameAndStatus(@Param("purchaser_name") String name, @Param("status") int status);
 
-    @Query(value = "SELECT * FROM pur_orders WHERE purchaser_name = :purchaser_name AND FIND_IN_SET(order_status, :statusSet) ORDER BY pur_serial_no LIMIT :cur, :pgSize", nativeQuery = true)
+    @Query(value = "SELECT * FROM pur_orders WHERE purchaser_name = :purchaser_name AND FIND_IN_SET(order_status, :statusSet) ORDER BY pur_serial_no DESC LIMIT :cur, :pgSize", nativeQuery = true)
     ArrayList<PurOrders> getPurOrderByNameAndStatus(@Param("purchaser_name") String name, @Param("statusSet") String statusSet, @Param("cur") int cur, @Param("pgSize") int pgSize);
 
     @Query(value = "SELECT * FROM pur_orders WHERE FIND_IN_SET(order_type, :provide_type)", nativeQuery = true)
